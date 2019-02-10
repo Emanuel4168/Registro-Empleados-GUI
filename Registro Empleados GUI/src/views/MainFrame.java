d2e9a95c6fafdc0e10ec33ebba54fe79a9e93b2d
Emanuel4168 <emanuel.l4168@gmail.com>
2019-02-10 09:05:12 -0700

    Creación de la parte gráfica del proyecto

---
+++ Registro Empleados GUI/src/views/MainFrame.java
@@ -0,0 +1,111 @@
+package views;
+
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.*;
+
+public class MainFrame extends JFrame implements ActionListener {
+
+	private JPanel northPanel,southPanel,centerPanel,westPanel,eastPanel;
+	private JRadioButton rbtNombre,rbtEdad,rbtEstatura,rbtAll;
+	private JButton btnRegistrar,btnConsulta,btnSalir;
+	private JTextField txtNombre, txtEdad,txtEstatura;
+	
+	public MainFrame() {
+		createNorthPanel();
+		createSouthPanel();
+		createCenterPanel();
+		setResizable(false);
+		setSize(400,300);
+		setLocationRelativeTo(null);
+		setVisible(true);
+		
+		crateListeners();
+		
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+	}
+	
+	public static void main(String [] a) {
+		new MainFrame();
+	}
+	
+	private void createNorthPanel() {
+		northPanel = new JPanel(); 
+		ButtonGroup group = new ButtonGroup();
+		rbtNombre = new JRadioButton("Nombre");
+		rbtEdad = new JRadioButton("Edad");
+		rbtEstatura = new JRadioButton("Estatura");
+		rbtAll = new JRadioButton("Edad-Estatura-Nombre");
+		
+		group.add(rbtNombre);
+		group.add(rbtEdad);
+		group.add(rbtEstatura);
+		group.add(rbtAll);
+		
+		northPanel.add(rbtNombre);
+		northPanel.add(rbtEdad);
+		northPanel.add(rbtEstatura);
+		northPanel.add(rbtAll);
+		
+		add(northPanel,BorderLayout.NORTH);
+	}
+	
+	private void createSouthPanel() {
+		southPanel = new JPanel();
+		
+		btnRegistrar = new JButton("Registrar");
+		btnConsulta = new JButton("Consultar");
+		btnSalir = new JButton("Salir");
+		
+		southPanel.add(btnRegistrar);
+		southPanel.add(btnConsulta);
+		southPanel.add(btnSalir);
+		
+		add(southPanel,BorderLayout.SOUTH);
+	}
+	
+	private void createCenterPanel() {
+		centerPanel = new JPanel(new GridLayout(0,2,10,10));
+				
+		txtNombre = new JTextField(20);
+		txtEdad = new JTextField(20);
+		txtEstatura = new JTextField(20);
+		
+		centerPanel.add(new JLabel("Nombre: ", SwingConstants.RIGHT));
+		centerPanel.add(txtNombre);
+		centerPanel.add(new JLabel("Edad: ", SwingConstants.RIGHT));
+		centerPanel.add(txtEdad);
+		centerPanel.add(new JLabel("Estatura: ", SwingConstants.RIGHT));
+		centerPanel.add(txtEstatura);
+		
+		add(centerPanel);
+	}
+
+	private void crateListeners() {
+		
+	}
+	
+	@Override
+	public void actionPerformed(ActionEvent e) {
+		if(e.getSource() instanceof JButton)
+			actionButton(e);
+		if(e.getSource() instanceof JRadioButton)
+			actionRadioButton(e);
+		if(e.getSource() instanceof JTextField)
+			actionTextField(e);
+	}
+	
+	private void actionButton(ActionEvent e) {
+		
+	}
+	
+	private void actionRadioButton(ActionEvent e) {
+		
+	}
+	
+	private void actionTextField(ActionEvent e) {
+		
+	}
+	
+}
